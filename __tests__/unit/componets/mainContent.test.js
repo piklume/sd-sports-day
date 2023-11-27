198f0191fc2106b8dae083dc1e37341465e7dfb3
Sayak Dutta <sayakdutta15@gmail.com>
2023-11-27 11:47:51 +0530

    test: add sports card unit tests

--- __tests__/unit/componets/mainContent.test.js
+++ __tests__/unit/componets/mainContent.test.js
@@ -1,11 +1,13 @@
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
+
 import MainContent from "src/app/components/mainContent.tsx";
+
 import { mockEvents } from "../../../fixtures/sportsEventsMock";
 
 describe("Main Content", () => {
-  it("Component renders", async () => {
-    render(<MainContent sportsEventList={mockEvents} />);
-    // expect(container).toBeDefined();
+  test("Component renders", async () => {
+    const { container } = render(<MainContent sportsEventList={mockEvents} />);
+    expect(container).toBeDefined();
   });
 });
---
+++ __tests__/unit/componets/sportsCard.test.js
@@ -0,0 +1,42 @@
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+
+import SportsCard from "src/app/components/sportsCard";
+
+import { mockEvents } from "../../../fixtures/sportsEventsMock";
+
+describe("Sports Card", () => {
+  const [mockCard] = mockEvents;
+
+  const mockOnClick = jest.fn();
+
+  const mockProps = {
+    card: mockCard,
+    onClick: mockOnClick,
+    ctaText: "Select",
+    isDisabled: false,
+  };
+
+  test("Component renders", () => {
+    const { container } = render(<SportsCard {...mockProps} />);
+    expect(container).toBeDefined();
+    expect(screen.getByText(mockCard.name)).toBeInTheDocument();
+    expect(screen.getByText(mockCard.category)).toBeInTheDocument();
+  });
+
+  test("Should call onClick on cta click if cta is not disabled", async () => {
+    const user = userEvent.setup();
+    render(<SportsCard {...mockProps} isDisabled={false} />);
+    const cta = screen.getByRole("button", { name: /Select/ });
+    await user.click(cta);
+    expect(mockOnClick).toHaveBeenCalled();
+  });
+
+  test("Should disable cta onclick when is disabled is true", async () => {
+    const user = userEvent.setup();
+    render(<SportsCard {...mockProps} isDisabled={true} />);
+    const cta = screen.getByRole("button", { name: /Select/ });
+    expect(cta).toBeDisabled();
+  });
+});
